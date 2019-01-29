499a89cce06f92f7be9ca310a214c1fcfb0b61b7
Sven Ruppert <sven.ruppert@gmail.com>
2019-01-29 17:26:20 +0100

    junit5 - 01 basics

--- src/test/java/junit/com/vaadin/tutorials/tdd/junit5/ServiceTest.java
+++ src/test/java/junit/com/vaadin/tutorials/tdd/junit5/ServiceTest.java
@@ -17,10 +17,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.condition.EnabledIf;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -87,73 +89,6 @@ public class ServiceTest {
               () -> assertEquals("Adolfo" , names.get(2)));
   }
 
-  @Test
-  void test007() {
-    Throwable runtimeException = assertThrows(RuntimeException.class , () -> {
-      throw new RuntimeException("exception");
-    });
-    assertEquals("exception" , runtimeException.getMessage());
-  }
-
-
-  //nested Tests - simple life cycle
-  private List<String> strings;
-
-  @Nested
-  class NestedTestClass {
-
-    @BeforeEach //only for this nested class
-    void init() {
-      strings = new ArrayList<>();
-    }
-
-    @Test
-    void listIsEmpty() {
-      assertTrue(strings.isEmpty());
-    }
-
-    @Nested //sub nested classes are possible
-    class afterAddingString {
-
-      @BeforeEach
-      void init() {
-        strings.add("hello");
-      }
-
-      @Test
-      void listIsNotEmpty() {
-        assertFalse(strings.isEmpty());
-      }
-    }
-  }
-
-
-  @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-  interface Interfaces{
-    @BeforeAll
-    default void initAll(TestReporter reporter){
-      reporter.publishEntry("@BeforeAll runs once before all tests");
-    }
-    @BeforeEach
-    default void init(TestReporter reporter){
-      reporter.publishEntry("@BeforeEach runs before each test");
-    }
-    @AfterAll
-    default void tearDownAll(TestReporter reporter){
-      reporter.publishEntry("@AfterAll runs once after all tests");
-    }
-    @AfterEach
-    default void tearDown(TestReporter reporter){
-      reporter.publishEntry("@AfterEach runs after each test");
-    }
-  }
-  @Nested
-  class ClassImplementingInterfaceTests implements Interfaces {
-    @Test
-    void myTest(TestReporter reporter){
-      reporter.publishEntry("the test itself");
-    }
-  }
 
 
 
